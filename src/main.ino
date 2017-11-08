85337c8847b77399d6d9372a6f285c5d577a60b3
Richard Osterloh <richard.osterloh@gmail.com>
2017-11-08 15:57:00 +0000

    Add schematic and SDD1306 display for debugging

--- src/main.ino
+++ src/main.ino
@@ -1,29 +1,37 @@
 #include <DHT.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_DotStar.h>
+#include <Adafruit_SSD1306.h>
 #include <SPI.h>
+#include <Wire.h>
 #include "wiring_private.h"
 
 #define LED_PIN                     13
-#define RELAY_PIN                   0
-#define DHT_PIN                     1
+#define RELAY_PIN                   3
+#define DHT_PIN                     4
 #define DHT_TYPE                    DHT22
 #define DHT_UPDATE_INTERVAL         60000
 #define DHT_TIMING                  11
 #define DATA_PIN                    7
 #define CLOCK_PIN                   8
 #define MISO_PIN                    30
+#define RESET_PIN                   1
+
+#undef SSD1306_128_32
+#define SSD1306_128_64
 
 DHT dht(DHT_PIN, DHT_TYPE, DHT_TIMING);
 Adafruit_DotStar pixel(1, DATA_PIN, CLOCK_PIN, DOTSTAR_BRG);
 //SPIClass ledSPI(&sercom1, MISO_PIN, CLOCK_PIN, DATA_PIN, SPI_PAD_0_SCK_1, SERCOM_RX_PAD_2);
 //Adafruit_DotStar pixel(1, &ledSPI, DOTSTAR_BRG); // Hardware SPI
+Adafruit_SSD1306 display(RESET_PIN);
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
   pinMode(RELAY_PIN, OUTPUT);
   dht.begin();
+
   //ledSPI.begin();
   //pinPeripheral(DATA_PIN, PIO_SERCOM);
   //pinPeripheral(CLOCK_PIN, PIO_SERCOM);
@@ -31,6 +39,10 @@ void setup() {
   pixel.begin();
   pixel.setBrightness(128); // 0 - 255
   pixel.show();
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
 
   for (int i = 0; i < 5; i++) {
     digitalWrite(LED_PIN, HIGH);
@@ -54,6 +66,10 @@ void loop() {
     } else {
       Serial.print("[DHT] Temperature: "); Serial.print(t); Serial.println(" ºC");
       Serial.print("[DHT] Humidity: "); Serial.print(h); Serial.println(" %");
+
+      display.clearDisplay();
+      display.print("T: "); display.print(t); display.println(" ºC");
+      display.print("H: "); display.print(h); display.println(" %");
 
       if (t > 22) {
         digitalWrite(RELAY_PIN, HIGH);
